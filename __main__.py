84932949a7236f5023d2f1c51ac5ebee6120a105
132nd-etcher <132nd-etcher@daribouca.net>
2018-03-22 19:41:53 +0100

    working on it

--- __main__.py
+++ __main__.py
@@ -8,9 +8,8 @@ import pathlib
 import os
 import elib
 
-_GIST_ID = r'7c70127508cf88ccc355bfbf67e2ea3d'
-_GIST_FILE_ID = r'fa381c9957e1eb5cab6b2c5cbb6cc013cc8b1731'
-YAML_LIST_URL = rf'https://gist.github.com/132nd-etcher/{_GIST_ID}/raw/{_GIST_FILE_ID}/yt.yml'
+
+YAML_LIST_URL = r'https://rawgit.com/132nd-etcher/7c70127508cf88ccc355bfbf67e2ea3d/raw/yt.yml'
 LOGGER = elib.custom_logging.get_logger('PITUBE', log_to_file=True, use_click_handler=False, console_level='DEBUG')
 
 
